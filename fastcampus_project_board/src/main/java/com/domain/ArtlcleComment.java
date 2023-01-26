e826ad85f8a94c03bf37538c266f4d0c9f145b37
camangbarry <camangbarry@gmail.com>
2023-01-26 21:50:29 +0900

    #9 - 도메인 클래스 설계

---
+++ fastcampus_project_board/src/main/java/com/domain/ArtlcleComment.java
@@ -0,0 +1,14 @@
+package com.domin;
+
+public class ArticleComment {
+    private Long id;
+    private Article article;
+    private String content;
+    private Sring hashtag;
+
+    private LocalDatetime creatAt;
+    private String createdBy;
+    private LocalDateTime modifiedAt;
+    private String modifiedBy;
+
+}
